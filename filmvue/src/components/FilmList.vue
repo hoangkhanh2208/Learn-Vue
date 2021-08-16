5aa881d10db5543c9b56a34b0420df3675cac723
hoangkhanh2208 <chuhoangkhanh00@gmail.com>
2021-08-16 21:38:19 +0700

    submit

---
+++ filmvue/src/components/FilmList.vue
@@ -0,0 +1,45 @@
+<template>
+
+   <table class="table">
+      <thead class="thead-dark">
+        <tr>
+            <th scope="col">#</th>
+            <th scope="col">Tên phim</th>
+            <th scope="col">Đạo diễn</th>
+            <th scope="col">Năm sản xuất</th>
+        </tr>
+        </thead>
+      <tbody id="array-rendering">
+        <tr v-for="(film, index) in films" :key="index" :film="film">
+          <th scope="row"></th>
+            <td>{{film.name}}</td>
+            <td>{{film.directors}}</td>
+            <td>{{film.year}}</td>
+        </tr>
+      </tbody>
+
+  </table>  
+
+  <!-- <ul id="array-rendering">
+    <li v-for="(film, index) in films" :key="index" :film="film">
+      {{ film.name + " : " + film.directors + " : " + film.year}}
+    </li>
+  </ul> -->
+</template>
+
+<script>
+export default {
+  props: {
+    films: {
+      type: Array,
+      required: true,
+    },
+  }
+}
+</script>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
---
+++ filmvue/src/main.js
@@ -0,0 +1,9 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+// import BootstrapVue from 'bootstrap-vue/dist/bootstrap-vue.esm'  
+import 'bootstrap/dist/css/bootstrap.css'
+// import "bootstrap-vue/dist/bootstrap-vue.css"
+
+// Vue.use(BootstrapVue);
+
+createApp(App).mount('#app')
